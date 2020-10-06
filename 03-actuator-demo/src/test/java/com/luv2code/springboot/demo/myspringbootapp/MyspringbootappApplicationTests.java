8cb2ee9beb8d6b218bcc6c28cb8f8c8bcf18136d
Supriyo Jana <47034828+hungryrag@users.noreply.github.com>
2020-10-06 18:43:01 +0530

    Spring boot crud demo-hibernate api

---
+++ 03-actuator-demo/src/test/java/com/luv2code/springboot/demo/myspringbootapp/MyspringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.myspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,84 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	// define field for entitymanager
+	private EntityManager entityManager;
+	
+	// set up constructor injection
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	public List<Employee> findAll() {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// create a query
+		Query<Employee> query = 
+				currentSession.createQuery("from Employee", Employee.class);
+		
+		// Execute query and get the result list
+		List<Employee> employees = query.getResultList();
+		
+		// return the result		
+		return employees;
+	}
+
+	@Override
+	public Employee findById(int id) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// get the employee
+		Employee employee = currentSession.get(Employee.class, id);
+		
+		// return the employee		
+		return employee;
+	}
+
+	@Override
+	public void save(Employee employee) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// save the employee
+		currentSession.saveOrUpdate(employee);
+		
+	}
+
+	@Override
+	public void deleteById(int id) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// delete object with primary key
+		Query query = currentSession.createQuery(
+				"delete from Employee where id=:employeeId");
+		
+		query.setParameter("employeeId", id);
+		
+		query.executeUpdate();
+		
+	}
+
+	
+	
+}
